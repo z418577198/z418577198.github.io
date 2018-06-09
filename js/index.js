63a5123714626e8ea078aa3285bc313521ed4b90
z418577198 <418577198@qq.com>
2018-06-09 17:08:15 +0800

    zzj

--- js/index.js
+++ js/index.js
@@ -4,7 +4,9 @@ $(function () {
         $('video').each(function () {
           var viewTop =  $(this).offset().top - $(window).scrollTop();
           if (viewTop < 100) {
-              $(this).get(0).play()
+              $(this).get(0).play();
+
+
           }
         })
 
